cf07c98a7b46e083d9025b5d696e8d38d277d366
Christopher Brown <io@henrian.com>
2013-07-12 14:00:44 -0500

    Ready for appfog3 branch -> appfog divvy3 app

--- settings.py
+++ settings.py
@@ -3,5 +3,5 @@ bind = '0.0.0.0:5080'
 accesslog = '-'
 errorlog = '-'
 loglevel = 'debug'  # e.g.: debug info warning error critical
-repo = dict(owner='chbrown', repo='divvy-history', branch='dates')
+repo = dict(owner='chbrown', repo='divvy-history', branch='appfog3')
 datadir = 'data'
